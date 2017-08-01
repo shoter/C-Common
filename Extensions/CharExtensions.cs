69d101d154736806713479693f000d765486ed54
shoter <damian>
2017-08-01 11:30:40 +0200

    Added CamelCaseToWrod in StringUtils and CharExtensions

---
+++ Extensions/CharExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Extensions
+{
+    public static class CharExtensions
+    {
+        /// <summary>
+        /// Change character to lowercase if able.
+        /// </summary>
+        public static char ToLower(this char character)
+        {
+            return char.ToLower(character);
+        }
+        /// <summary>
+        /// Change character to uppercase if able.
+        /// </summary>
+        public static char ToUpper(this char character)
+        {
+            return char.ToUpper(character);
+        }
+
+        /// <summary>
+        /// returns true if character is uppercase
+        /// </summary>
+        public static bool IsUpper(this char character)
+        {
+            return char.IsUpper(character);
+        }
+
+        /// <summary>
+        /// returns true if character is lowercase
+        /// </summary>
+        public static bool IsLower(this char character)
+        {
+            return char.IsLower(character);
+        }
+
+
+    }
+}
--- utilities/StringUtils.cs
+++ utilities/StringUtils.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Common.Extensions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -30,5 +31,33 @@ namespace Common.utilities
 
             return changed;
         }
+
+        /// <summary>
+        /// Change camel case word to words.
+        /// For example BeAble will be changed to "be able"
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string CamelCaseToWord(this string str)
+        {
+            string changed = "";
+            
+            if(str.Length > 0)
+            {
+                changed += str[0].ToLower();
+                for(int i = 1; i < str.Length;++i)
+                {
+                    char character = str[i];
+
+                    if (character.IsUpper())
+                        changed += " ";
+                    changed += character.ToLower();
+                    
+                }
+            }
+
+            return changed;
+        }
+
     }
 }
