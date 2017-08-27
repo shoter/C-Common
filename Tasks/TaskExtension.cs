dc566f7b15488b5a93d1037d7fca7946a2d75b26
shoter <damian>
2017-08-27 20:00:13 +0200

    Added Closeable TabItem and RunAsync for Task.

---
+++ Tasks/TaskExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Tasks
+{
+    public static class TaskExtension
+    {
+        ///https://stackoverflow.com/a/17224745/2583946
+        /// <summary> 
+        /// Runs the Task in a concurrent thread without waiting for it to complete. This will start the task if it is not already running. 
+        /// </summary> 
+        /// <param name="task">The task to run.</param> 
+        /// <remarks>This is usually used to avoid warning messages about not waiting for the task to complete.</remarks> 
+        public static void RunAsync(this Task task)
+        {
+            if (task == null)
+                throw new ArgumentNullException("task", "task is null.");
+
+
+            if (task.Status == TaskStatus.Created)
+                task.Start();
+        }
+    }
+}
---
+++ WPF/Controls/CloseableTabItem.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Common.WPF.Controls
+{
+    public class ClosableTab : TabItem
+    {
+        // Constructor
+        public ClosableTab()
+        {
+            // Create an instance of the usercontrol
+            var closableTabHeader = new CloseableHeader();
+            // Assign the usercontrol to the tab header
+            this.Header = closableTabHeader;
+            
+            closableTabHeader.CloseButton.MouseEnter +=
+   new MouseEventHandler(button_close_MouseEnter);
+            closableTabHeader.CloseButton.MouseLeave +=
+               new MouseEventHandler(button_close_MouseLeave);
+            closableTabHeader.CloseButton.Click +=
+               new RoutedEventHandler(button_close_Click);
+            closableTabHeader.CloseButton.SizeChanged +=
+               new SizeChangedEventHandler(label_TabTitle_SizeChanged);
+        }
+       
+        public string Title
+        {
+            set
+            {
+                ((CloseableHeader)this.Header).Header.Content = value;
+            }
+        }
+
+        protected override void OnSelected(RoutedEventArgs e)
+        {
+            base.OnSelected(e);
+            ((CloseableHeader)this.Header).CloseButton.Visibility = Visibility.Visible;
+        }
+
+        protected override void OnUnselected(RoutedEventArgs e)
+        {
+            base.OnUnselected(e);
+            ((CloseableHeader)this.Header).CloseButton.Visibility = Visibility.Hidden;
+        }
+
+        protected override void OnMouseEnter(MouseEventArgs e)
+        {
+            base.OnMouseEnter(e);
+            ((CloseableHeader)this.Header).CloseButton.Visibility = Visibility.Visible;
+        }
+
+        protected override void OnMouseLeave(MouseEventArgs e)
+        {
+            base.OnMouseLeave(e);
+            if (!this.IsSelected)
+            {
+                ((CloseableHeader)this.Header).CloseButton.Visibility = Visibility.Hidden;
+            }
+        }
+
+        // Button MouseEnter - When the mouse is over the button - change color to Red
+        void button_close_MouseEnter(object sender, MouseEventArgs e)
+        {
+            ((CloseableHeader)this.Header).CloseButton.Foreground = Brushes.Red;
+        }
+        // Button MouseLeave - When mouse is no longer over button - change color back to black
+        void button_close_MouseLeave(object sender, MouseEventArgs e)
+        {
+            ((CloseableHeader)this.Header).CloseButton.Foreground = Brushes.Black;
+        }
+        // Button Close Click - Remove the Tab - (or raise
+        // an event indicating a "CloseTab" event has occurred)
+        void button_close_Click(object sender, RoutedEventArgs e)
+        {
+            ((TabControl)this.Parent).Items.Remove(this);
+        }
+        // Label SizeChanged - When the Size of the Label changes
+        // (due to setting the Title) set position of button properly
+        void label_TabTitle_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            ((CloseableHeader)this.Header).CloseButton.Margin = new Thickness(
+               ((CloseableHeader)this.Header).CloseButton.ActualWidth + 5, 3, 4, 0);
+        }
+    }
+}
