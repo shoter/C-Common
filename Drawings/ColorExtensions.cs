01fff3b262ddd02e9cd124bc15236494dcc01f69
shoter <lubiebardzozelki@gmail.com>
2017-09-14 00:57:40 +0200

    Added Color Lerp and Color.ToHex()

---
+++ Drawings/ColorExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Color = System.Drawing.Color;
+
+namespace Common.Drawings
+{
+    public static class ColorExtensions
+    {
+        public static string ToHex(this Color color)
+        {
+            return "#" + color.R.ToString("X2") + color.G.ToString("X2") + color.B.ToString("X2");
+        }
+
+    }
+}
---
+++ Drawings/ColorInterpolator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Color = System.Drawing.Color;
+
+namespace Common.Drawings
+{
+    // http://stackoverflow.com/questions/1236683/color-interpolation-between-3-colors-in-net
+    public static class ColorInterpolator
+    {
+        delegate byte ComponentSelector(Color color);
+        static ComponentSelector _redSelector = color => color.R;
+        static ComponentSelector _greenSelector = color => color.G;
+        static ComponentSelector _blueSelector = color => color.B;
+
+        public static Color Lerp(
+            Color endPoint1,
+            Color endPoint2,
+            double lambda)
+        {
+            if (lambda < 0 || lambda > 1)
+            {
+                throw new ArgumentOutOfRangeException("lambda");
+            }
+            Color color = Color.FromArgb(
+                InterpolateComponent(endPoint1, endPoint2, lambda, _redSelector),
+                InterpolateComponent(endPoint1, endPoint2, lambda, _greenSelector),
+                InterpolateComponent(endPoint1, endPoint2, lambda, _blueSelector)
+            );
+
+            return color;
+        }
+
+        private static byte InterpolateComponent(
+            Color endPoint1,
+            Color endPoint2,
+            double lambda,
+            ComponentSelector selector)
+        {
+            return (byte)(selector(endPoint1)
+                + (selector(endPoint2) - selector(endPoint1)) * lambda);
+        }
+    }
+}
