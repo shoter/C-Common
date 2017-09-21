6e83bd9a464194db27e87ea9339f0a16b3204e44
shoter <lubiebardzozelki@gmail.com>
2017-09-21 07:22:49 +0200

    Added string extensions

---
+++ Extensions/StringExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string FirstUpper(this string str)
+        {
+            if (str?.Length > 1)
+            {
+                return ReplaceAt(str, 0, str[0].ToUpper());
+            }
+            return str;
+        }
+
+        public static string ReplaceAt(this string input, int index, char newChar)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException("input");
+            }
+            char[] chars = input.ToCharArray();
+            chars[index] = newChar;
+            return new string(chars);
+        }
+    }
+}
