b94dd099aa8baec11f65ee8541a20c9bdf1f59e2
shoter <lubiebardzozelki@gmail.com>
2017-10-25 12:38:58 +0200

    Added JavascriptDate with tests

---
+++ CommonTests/Utilities/JavascriptDateTests.cs
@@ -0,0 +1,47 @@
+﻿using Common.utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonTests.Utilities
+{
+    [TestClass]
+    public class JavascriptDateTests
+    {
+        [TestMethod]
+        public void JavascriptTimeStartTest()
+        {
+            var date = new JavascriptDate(javascriptMilliseconds: 0);
+
+            Assert.AreEqual(1, date.Date.Day);
+            Assert.AreEqual(1, date.Date.Month);
+            Assert.AreEqual(1970, date.Date.Year);
+            Assert.AreEqual(0, date.JavascriptMilliseconds);
+        }
+
+        [TestMethod]
+        public void JavascriptTimeStartFromNetTest()
+        {
+            var date = new JavascriptDate(new DateTime(1970, 1, 1));
+
+            Assert.AreEqual(1, date.Date.Day);
+            Assert.AreEqual(1, date.Date.Month);
+            Assert.AreEqual(1970, date.Date.Year);
+            Assert.AreEqual(0, date.JavascriptMilliseconds);
+        }
+
+        [TestMethod]
+        public void JavascriptTimeStartFromJavascriptToNet()
+        {
+            var date = new JavascriptDate(new JavascriptDate(javascriptMilliseconds: 0).Date);
+
+            Assert.AreEqual(1, date.Date.Day);
+            Assert.AreEqual(1, date.Date.Month);
+            Assert.AreEqual(1970, date.Date.Year);
+            Assert.AreEqual(0, date.JavascriptMilliseconds);
+        }
+    }
+}
---
+++ utilities/JavascriptDate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.utilities
+{
+    public class JavascriptDate
+    {
+        const long epochMiliseconds = 62135596800000;
+        const long ticksPerMillisecond = 10_000;
+
+        public readonly long JavascriptMilliseconds;
+        public readonly DateTime Date;
+
+
+        /// <param name="date">.NET date</param>
+        public JavascriptDate(DateTime date)
+        {
+            Date = date;
+            JavascriptMilliseconds = date.ToUniversalTime().Millisecond;
+        }
+
+        /// <param name="javascriptMilliseconds">milliseconds from date.GetTime()</param>
+        public JavascriptDate(long javascriptMilliseconds)
+        {
+            JavascriptMilliseconds = javascriptMilliseconds;
+            Date = new DateTime(epochMiliseconds * ticksPerMillisecond, DateTimeKind.Utc);
+        }
+    }
+}
