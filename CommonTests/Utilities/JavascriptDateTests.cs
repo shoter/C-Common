2d4af527505e6d2bb02f4d8bbb5cf685a296f65c
shoter <lubiebardzozelki@gmail.com>
2017-11-01 14:47:46 +0100

    JavascriptDate class. Ugly... and unfisnihed.

--- CommonTests/Utilities/JavascriptDateTests.cs
+++ CommonTests/Utilities/JavascriptDateTests.cs
@@ -16,32 +16,102 @@ namespace CommonTests.Utilities
         {
             var date = new JavascriptDate(javascriptMilliseconds: 0);
 
-            Assert.AreEqual(1, date.Date.Day);
-            Assert.AreEqual(1, date.Date.Month);
-            Assert.AreEqual(1970, date.Date.Year);
+            Assert.AreEqual(1, date.UtcDate.Day);
+            Assert.AreEqual(1, date.UtcDate.Month);
+            Assert.AreEqual(1970, date.UtcDate.Year);
             Assert.AreEqual(0, date.JavascriptMilliseconds);
         }
 
         [TestMethod]
         public void JavascriptTimeStartFromNetTest()
         {
-            var date = new JavascriptDate(new DateTime(1970, 1, 1));
+            var date = new JavascriptDate(new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc));
 
-            Assert.AreEqual(1, date.Date.Day);
-            Assert.AreEqual(1, date.Date.Month);
-            Assert.AreEqual(1970, date.Date.Year);
+            var local = date.Local;
+
+            Assert.AreEqual(1, local.Day);
+            Assert.AreEqual(1, local.Month);
+            Assert.AreEqual(1970, local.Year);
             Assert.AreEqual(0, date.JavascriptMilliseconds);
         }
 
-        [TestMethod]
-        public void JavascriptTimeStartFromJavascriptToNet()
-        {
-            var date = new JavascriptDate(new JavascriptDate(javascriptMilliseconds: 0).Date);
+        //[TestMethod]
+        //public void JavascriptTimeStartFromJavascriptToNet()
+        //{
+        //    var date = new JavascriptDate(new JavascriptDate(javascriptMilliseconds: 0).Date);
 
-            Assert.AreEqual(1, date.Date.Day);
-            Assert.AreEqual(1, date.Date.Month);
-            Assert.AreEqual(1970, date.Date.Year);
-            Assert.AreEqual(0, date.JavascriptMilliseconds);
+        //    var local = date.Date.ToLocalTime();
+
+        //    Assert.AreEqual(1, local.Day);
+        //    Assert.AreEqual(1, local.Month);
+        //    Assert.AreEqual(1970, local.Year);
+        //    Assert.AreEqual(0, date.JavascriptMilliseconds);
+        //}
+
+        [TestMethod]
+        public void JavascriptSpecificDateTest()
+        {
+            var tick = 980985600000;
+
+            var date = new JavascriptDate(tick);
+
+            Assert.AreEqual(0, date.UtcDate.Second);
+            Assert.AreEqual(0,date.UtcDate.Minute);
+            Assert.AreEqual(0,date.UtcDate.Hour);
+            Assert.AreEqual(1, date.UtcDate.Day);
+            Assert.AreEqual(2, date.UtcDate.Month);
+            Assert.AreEqual(2001, date.UtcDate.Year);
+        }
+
+        [TestMethod]
+        public void JavascriptSpecificDateTest2()
+        {
+            var tick = 1112670330000;
+
+            var date = new JavascriptDate(tick);
+
+            Assert.AreEqual(30, date.UtcDate.Second);
+            Assert.AreEqual(5, date.UtcDate.Minute);
+            Assert.AreEqual(3, date.UtcDate.Hour);
+            Assert.AreEqual(5, date.UtcDate.Day);
+            Assert.AreEqual(4, date.UtcDate.Month);
+            Assert.AreEqual(2005, date.UtcDate.Year);
+        }
+
+        [TestMethod]
+        public void JavascriptSpecificTick2Test()
+        {
+            var tick = 1112670330000;
+
+            var date = new JavascriptDate(new DateTime(2005, 3, 5, 3, 5, 30));
+
+            Assert.AreEqual(tick, date.JavascriptMilliseconds);
+        }
+
+        [TestMethod]
+        public void JavascriptSpecificTickTest()
+        {
+            var tick = 980985600000;
+
+            var date = new JavascriptDate(new DateTime(2001, 2, 1, 0, 0, 0));
+
+            Assert.AreEqual(tick, date.JavascriptMilliseconds);
+        }
+
+        [TestMethod]
+        public void JavascriptCurrentDateTest()
+        {
+            var tick = 1509089730938;
+
+            var date = new JavascriptDate(tick);
+
+            var local = date.Local;
+
+            Assert.AreEqual(35, local.Minute);
+            Assert.AreEqual(9, local.Hour);
+            Assert.AreEqual(27, local.Day);
+            Assert.AreEqual(10, local.Month);
+            Assert.AreEqual(2017, local.Year);
         }
     }
 }
--- utilities/JavascriptDate.cs
+++ utilities/JavascriptDate.cs
@@ -12,21 +12,32 @@ namespace Common.utilities
         const long ticksPerMillisecond = 10_000;
 
         public readonly long JavascriptMilliseconds;
-        public readonly DateTime Date;
+        public readonly DateTime UtcDate;
+        public DateTime Local => UtcDate.ToLocalTime();
 
 
         /// <param name="date">.NET date</param>
         public JavascriptDate(DateTime date)
         {
-            Date = date;
-            JavascriptMilliseconds = date.ToUniversalTime().Millisecond;
+            if (date.Kind != DateTimeKind.Utc)
+            {
+                date = date.ToUniversalTime();
+            }
+
+            UtcDate = date;
+            JavascriptMilliseconds = (long)date.
+                Subtract(new DateTime(1970, 1, 1).ToUniversalTime())
+                .Subtract(new TimeSpan(1, 0, 0))
+                .TotalMilliseconds;
         }
 
         /// <param name="javascriptMilliseconds">milliseconds from date.GetTime()</param>
         public JavascriptDate(long javascriptMilliseconds)
         {
             JavascriptMilliseconds = javascriptMilliseconds;
-            Date = new DateTime(epochMiliseconds * ticksPerMillisecond, DateTimeKind.Utc);
+            UtcDate = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)
+                .AddMilliseconds(javascriptMilliseconds);
+            //Date = new DateTime(epochMiliseconds * ticksPerMillisecond + javascriptMilliseconds * ticksPerMillisecond, DateTimeKind.Utc);
         }
     }
 }
