bf139dbab5869b85fcfa7e00dc187a40e3a0c962
shoter <lubiebardzozelki@gmail.com>
2017-11-04 23:03:53 +0100

    Added Random enum value picker and Random boolean value.

---
+++ Enums.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Enums
+    {
+        public static Random random = new Random();
+
+        public static TEnum GetRandomValue<TEnum>()
+        {
+            TEnum[] values = Enum.GetValues(typeof(TEnum)).Cast<TEnum>().ToArray();
+            return values[random.Next(0, values.Length)];
+        }
+    }
+}
---
+++ Extensions/RandomExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Extensions
+{
+    public static class RandomExtensions
+    {
+        public static bool NextBoolean(this Random random)
+        {
+            return random.Next(0, 2) == 1 ? true : false;
+        }
+    }
+}
